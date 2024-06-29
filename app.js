1e96d207ecb5db8f193812a23d155c47512fb7a7
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-06-29 15:57:31 -0700

    Implement changing slides with dots & make mobile responsive

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 let slideNum = 0;
 const allSlides = document.querySelectorAll(".slide");
-console.log(allSlides.length);
+
 function nextSlide() {
   if (slideNum !== allSlides.length - 1) showSlide(++slideNum);
-  console.log("Going to next slide!", "slideNum is: ", slideNum);
 }
 
 function prevSlide() {
   if (slideNum !== 0) {
     showSlide(--slideNum);
-    console.log("Going to prev slide!", "slideNum is: ", slideNum);
   }
 }
 
 function showSlide(num) {
-  allSlides.forEach((slide, index) => {
+  allSlides.forEach((slide) => {
     if (!slide.classList.contains("hidden")) {
       slide.classList.toggle("hidden");
     }
   });
   allSlides[num].classList.toggle("hidden");
+  activateDot(num);
+}
+
+function activateDot(num) {
+  const allDots = document.querySelectorAll(".dot");
+  allDots.forEach((dot) => {
+    if (dot.classList.contains("active")) {
+      dot.classList.toggle("active");
+    }
+  });
+  allDots[num].classList.toggle("active");
+  slideNum = num;
 }
